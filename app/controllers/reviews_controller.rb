8b978e3774dd7eb651e1868902ec2fb5afdafe4c
mag <mag.it.use@gmail.com>
2018-09-17 20:07:57 +0000

    created a route to handle the create (POST) request, generated the ReviewsController to handle the create action.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @review.product_id = params[:product_id]
+    @review.user = current_user
+
+    if @review.save
+        redirect_to product_path(@review.product)
+      else
+        redirect_to @review.product
+      end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
+  end
+end
